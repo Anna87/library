e023971504d018c30378979e3e650d70972472a5
alexpench <alex.pench@gmail.com>
2019-05-30 18:40:12 +0200

    error handle

--- src/main/java/com/customer/java/services/Sender.java
+++ src/main/java/com/customer/java/services/Sender.java
@@ -3,7 +3,6 @@ package com.customer.java.services;
 import com.customer.java.models.Borrow;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jms.core.JmsTemplate;
-import org.springframework.scheduling.annotation.Scheduled;
 
 import java.util.Date;
 import java.util.List;
@@ -16,7 +15,7 @@ public class Sender {
     BorrowService borrowService;
 
     // @Scheduled(cron = "0 0 12 * * ?") //At 12:00 pm (noon) every day
-    @Scheduled(cron = "5 * * * * ?") //Every minutes
+    //@Scheduled(cron = "5 * * * * ?") //Every minutes
     public void scheduleTask() {
 
         Date date = new Date();
