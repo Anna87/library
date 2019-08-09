a9c4b8d43f54249e1d361767289b1ea6b4f3e97f
alexpench <alex.pench@gmail.com>
2019-08-09 14:30:17 +0200

    repackage

--- src/main/java/com/library/java/Application.java
+++ src/main/java/com/library/java/Application.java
@@ -10,7 +10,6 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableEurekaClient
 @EnableFeignClients
 @EnableScheduling
-//@ComponentScan
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class);
